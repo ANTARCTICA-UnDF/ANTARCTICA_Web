793c7be44efbf724dd36e9f6b6c2fbf87d5686c0
Alex Silva <desufg826@gmail.com>
2025-04-18 20:20:40 -0300

    Primeiras alterações + Aprendizado Geral

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+  <h1>Bem-vindo ao ANTARCTICA Web!</h1>
+  <p>Este é o início da sua jornada com React 🧊</p>
+  <Footer />
+</>
+
+  )
+}
+
+export default App
